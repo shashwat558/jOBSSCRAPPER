d2caebe73b2d39200e015d9db9a3d62c31c98e6a
Shashwat <tumbaktuhere@gmail.com>
2023-09-18 11:45:40 +0530

    Jobs Scrapper

---
+++ real.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+
+print('Put some skill that you are not familiar with')
+unfamilier_skill = input('>')
+print(f'Filtering Out {unfamilier_skill}')
+def find_jobs():
+    html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=Python&txtLocation=').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx') 
+    for index, job in enumerate(jobs):
+        publishdates =  job.find('span', class_ = 'sim-posted').span.text
+        if 'few' in publishdates:
+            company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+            skills = job.find('span', class_ = 'srp-skills').text.replace(' ', '')
+            moreInfo = job.header.h2.a['href']
+            if  unfamilier_skill not in skills:
+                with open(f'posts/{index}.txt', 'w') as f:
+                    f.write(f"Company Name: {company_name.strip()}\n")
+                    f.write(f"Required Skills: {skills.strip()}\n")
+                    f.write(f"More Info: {moreInfo}")
+                print(f'File saved: {index}')
+
+if __name__ == '__main__':
+    while True:
+        find_jobs()
+        time_wait = 10
+        print(f'Waiting {time_wait} minutes... ')
+        time.sleep(time_wait * 60)
+
+
